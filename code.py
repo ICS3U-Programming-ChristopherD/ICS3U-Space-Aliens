6b9e33b50eb4040ad71cacd2e0dd3e4fdea38351
Christopher <92882782+Krostofer@users.noreply.github.com>
2023-01-11 14:13:51 -0500

    Update code.py

--- code.py
+++ code.py
@@ -4,4 +4,23 @@
 # Date: Jan. 11, 2023
 # This prints "Hello, World!" to the screen
 
-print("\n\n\n\n\n\nHello, World!")
+import ugame
+import stage
+
+
+def game_scene():
+
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+    # Repeats forever
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
