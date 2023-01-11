30785d12be23cbabe8ab7d8f215e650d6dc8ca26
Christopher <92882782+Krostofer@users.noreply.github.com>
2023-01-11 16:10:11 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
-
+ 
 # Created By: Chris Di Bert
 # Date: Jan. 11, 2023
 # This prints "Hello, World!" to the screen
-
-import stage
+ 
 import ugame
-
+import stage
+ 
+ 
 def game_scene():
-
+ 
+# Image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+# Sets the background image to image 0 in the image bank and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
-
+ 
+# Create a stage for the background to show up on and set
+# the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
+ 
+# Set the layers, items show up in order
     game.layers = [background]
+ 
+# Render the background and sprites
     game.render_block()
-
+ 
     # Repeats forever
     while True:
         pass
-
-
+ 
+ 
 if __name__ == "__main__":
     game_scene()
